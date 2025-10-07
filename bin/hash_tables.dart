c6d2227cda5862857e437be6454e2b87fdef9efa
kittywxx <smirnova.ccat@gmail.com>
2025-10-07 12:52:55 +0800

    Preparation for hash

--- bin/hash_tables.dart
+++ bin/hash_tables.dart
@@ -1,4 +1,5 @@
 import 'package:hash_tables/binary_search.dart';
+import 'package:hash_tables/hash_search.dart';
 import 'package:hash_tables/linear_search.dart';
 
 void main() {
@@ -12,4 +13,27 @@ void main() {
 
   final foundTarget = binarySearch(myList, 9);
   print(foundTarget);
+
+  final studebtList = [
+    Student(2024078, 'AZ2'),
+    Student(2024077, 'AZ2'),
+    Student(2021018, 'AD'),
+    Student(2023139, 'BB'),
+    Student(2025068, 'BS'),
+    Student(2024008, 'DD'),
+    Student(2024095, 'ES1'),
+    Student(2024012, 'ED1'),
+    Student(2024054, 'EB'),
+    Student(2024081, 'ED2'),
+    Student(2024111, 'ES2'),
+    Student(2024059, 'MI'),
+    Student(2024025, 'MB'),
+    Student(2024020, 'MC'),
+    Student(2023002, 'NM'),
+    Student(2022081, 'TG'),
+    Student(2024085, 'TK'),
+    Student(2023096, 'TA'),
+    Student(2022007, 'UM'),
+    Student(2024007, 'VG')
+  ];
 }
